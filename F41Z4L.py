8d47326aa06b72892ecd140e67e97215ee2975c7
f41z4l <48168686+f41z4l@users.noreply.github.com>
2019-03-05 18:58:11 +0700

    Add files via upload

---
+++ F41Z4L.py
@@ -0,0 +1,39 @@
+print ("|----------------------------|")
+print ("{ AUTHOR   : F41Z4L          }")
+print ("{ WA       : 085749******    }")
+print ("{ FACEBOOK : ??              }")
+print ("{ FUNGSI   : HACK FB TARGET  }")
+print ("{  JANGAN RECODE YAA KNTL    }")
+print ("{     GW CAPEK BUATNYA       }")
+print ("|----------------------------|")
+
+#variabel
+text=('\033[1;33mwelcome to my tools')
+print(text)
+
+#input output
+nama=input('masukkan nama anda : ')
+print("\033[1;37mselamat datang",nama)
+
+menu='''
+1. mulai
+
+'''
+print(menu)
+text1=input('masukkan pilihan anda : ')
+if text1 == '1':
+   lagi=input("MASUKKAN ID TARGET : ")
+   print("sedang mengambil data korban...")
+
+import os
+import sys
+import time
+os.system ('sleep 4')
+print("\033[1;31mNAME      : NOT FOUND ")
+print("EMAIL     : NOT FOUND ")
+print("PHONE     : NOT FOUND ")
+print("PASSWORD  : NOT FOUND ")
+print("LOCATIONS : NOT FOUND ")
+print("STATUS    : NOT FOUND ")
+print("\033[1;34m《TERTIPU KAU KNTL》")
+print("\033[1;36m《《《 TTD F41Z4L_GANS 》》》")
